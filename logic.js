bf2ed8b89e87cbd0a89efed47664e0d506a8e17c
mariusbike1983 <pepumariusdoru@gmail.com>
2023-03-04 10:11:24 +0200

    prevent adding empty todos

--- logic.js
+++ logic.js
@@ -4,10 +4,16 @@ window.addEventListener('load', function() {
         d.addEventListener('click', handleClickOnItem);
     });
     
-	document.getElementById('add-button').addEventListener('click', function() {
+	document.getElementById('add-button').addEventListener('click', function(){
 		const todoText = prompt("What do you plan on doing?");
+		if (!todoText) {
+			return;
+		}
 		const when = prompt("And when do you plan on doing "+todoText+"?");
-        const newItem = document.createElement("div");
+		if (!when) {
+			return;
+		}
+		const newItem = document.createElement("div");
 		newItem.classList.add("todo-item");
 		newItem.innerText = todoText;
 
@@ -20,16 +26,16 @@ window.addEventListener('load', function() {
 
 		const inProgressList = document.getElementById('current-todo-list');
 		inProgressList.appendChild(newItem);
-    });
-});
+	});
 
-function handleClickOnItem(event) {
-	const currentItem = event.target;
-	const completedList = document.getElementById('completed-todo-list');
-	const inProgressList = document.getElementById('current-todo-list');
-	if (currentItem.parentElement === completedList) {
-		inProgressList.appendChild(currentItem);
-	} else {
-		completedList.appendChild(currentItem);
-	}
-};
+	function handleClickOnItem(event) {
+		const currentItem = event.target;
+		const completedList = document.getElementById('completed-todo-list');
+		const inProgressList = document.getElementById('current-todo-list');
+		if (currentItem.parentElement === completedList) {
+			inProgressList.appendChild(currentItem);
+		} else {
+			completedList.appendChild(currentItem);
+		}
+	};
+});
