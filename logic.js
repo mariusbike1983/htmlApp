e8b17b0ac380474e2c2e340329ecf91f07e937f2
mariusbike1983 <pepumariusdoru@gmail.com>
2023-03-04 10:01:09 +0200

    added functionality to add a new element dynamically

--- logic.js
+++ logic.js
@@ -4,15 +4,29 @@ window.addEventListener('load', function() {
         d.addEventListener('click', handleClickOnItem);
     });
     
-	document.getElementById('add-button').addEventListener('click', function(){
-        alert('clicked the add button');
+	document.getElementById('add-button').addEventListener('click', function() {
+		const todoText = prompt("What do you plan on doing?");
+		const when = prompt("And when do you plan on doing "+todoText+"?");
+        const newItem = document.createElement("div");
+		newItem.classList.add("todo-item");
+		newItem.innerText = todoText;
+
+		const detail = document.createElement("span");
+		detail.classList.add("detail");
+		detail.innerText = when;
+
+		newItem.appendChild(detail);
+		newItem.addEventListener('click', handleClickOnItem);
+
+		const inProgressList = document.getElementById('current-todo-list');
+		inProgressList.appendChild(newItem);
     });
 });
 
 function handleClickOnItem(event) {
-	let currentItem = event.target;
-	let completedList = document.getElementById('completed-todo-list');
-	let inProgressList = document.getElementById('current-todo-list');
+	const currentItem = event.target;
+	const completedList = document.getElementById('completed-todo-list');
+	const inProgressList = document.getElementById('current-todo-list');
 	if (currentItem.parentElement === completedList) {
 		inProgressList.appendChild(currentItem);
 	} else {
